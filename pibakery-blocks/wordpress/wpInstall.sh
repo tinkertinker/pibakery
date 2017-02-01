583ba3ecd775160ad9b3f7134edd98fb053cb080
George Hotelling <george.hotelling@automattic.com>
2017-02-01 19:45:10 +0100

    Switch to wp-cli

--- pibakery-blocks/wordpress/wpInstall.sh
+++ pibakery-blocks/wordpress/wpInstall.sh
@@ -1,28 +1,24 @@
-#!/bin/bash
-apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get -y install wordpress apache2 mysql-server php5 php5-mysql
+### trigger mdns fix!!!!
 
-echo "Alias /wp/wp-content /var/lib/wordpress/wp-content
-Alias /wp /usr/share/wordpress
-<Directory /usr/share/wordpress>
-  Options FollowSymLinks
-  AllowOverride Limit Options FileInfo
-  DirectoryIndex index.php
-  Require all granted
-</Directory>
-<Directory /var/lib/wordpress/wp-content>
-  Options FollowSymLinks
-  Require all granted
-</Directory>" > /etc/apache2/sites-available/wp.conf
-a2ensite wp
-service apache2 reload
+DEBIAN_FRONTEND=noninteractive apt-get -y install python-pygame sense-hat
 
-mysql -u root -e "CREATE DATABASE wordpress"
-mysqladmin -u root password "$1"
+mysql -u root --password="$1" -e "CREATE DATABASE wordpress"
 
-echo "<?php
-define('DB_NAME', 'wordpress');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '$1');
-define('DB_HOST', 'localhost');
-define('WP_CONTENT_DIR', '/var/lib/wordpress/wp-content');" > /etc/wordpress/config-default.php
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+
+rm /var/www/html/index.html
+chgrp -R pi /var/www/html
+chmod -R g+w /var/www/html
+
+sudo -u pi -i -- wp core download --path=/var/www/html
+sudo -u pi -i -- wp core config --path=/var/www/html --dbname=wordpress --dbuser=root --dbpass="$1"
+sudo -u pi -i -- wp core install --path=/var/www/html --url=http://$(hostname).local/ --title="Blog In A Box" --admin_user="$2" --admin_email="email@example.com" --admin_password="$3" --skip-email
+PLUGIN_LOCATION=`curl -I https://api.github.com/repos/tinkertinker/biab/zipball | perl -n -e '/^Location: (.*)$/ && print "$1\n"'`
+sudo -u pi -i -- wp plugin install --path=/var/www/html $PLUGIN_LOCATION --activate
+THEME_LOCATION=`curl -I https://api.github.com/repos/tinkertinker/biab-theme/zipball | perl -n -e '/^Location: (.*)$/ && print "$1\n"'`
+sudo -u pi -i -- wp theme install --path=/var/www/html $THEME_LOCATION --activate
+
+# install cron job
+sudo -u pi -i -c '(crontab -l ; echo "* * * * * wp cron event run --due-now --path=/var/www/html") | crontab -'
